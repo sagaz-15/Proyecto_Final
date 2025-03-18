94131e9164e17af81c0d15ab493d355282f2b8b2
santiago <147778902+sagaz-15@users.noreply.github.com>
2025-03-18 09:44:44 -0500

    Add files via upload

---
+++ Proyecto/src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto;
+
+/**
+ *
+ * @author robin
+ */
+public class Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
