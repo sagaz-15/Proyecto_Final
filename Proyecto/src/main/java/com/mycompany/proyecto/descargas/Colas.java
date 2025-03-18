caf933966b5e85c2440f62f3db19f41d523dc6b0
santiago <147778902+sagaz-15@users.noreply.github.com>
2025-03-18 09:54:41 -0500

    Update Colas.java

--- Proyecto/src/main/java/com/mycompany/proyecto/descargas/Colas.java
+++ Proyecto/src/main/java/com/mycompany/proyecto/descargas/Colas.java
@@ -8,6 +8,50 @@ package com.mycompany.proyecto.descargas;
  *
  * @author robin
  */
+import java.util.Arrays;
+
 public class Colas {
-    
+    private int numElememtos;
+    private int inicio;
+    private int end;
+    private String[] cola;
+
+    // Constructor
+    public Colas(int tam) {
+        cola = new String[tam];
+        this.numElememtos = 0;
+        this.inicio = 0;
+        this.end = -1;
+    }
+
+    public boolean vacia() {
+        return numElememtos == 0;
+    }
+
+    public boolean insertar(String elemento) {
+        if (numElememtos < cola.length) {
+            end = (end + 1) % cola.length;
+            cola[end] = elemento;
+            numElememtos++;
+            return true;
+        }
+        return false;
+    }
+
+    public String remover() {
+        if (vacia()) {
+            return null;
+        }
+        String eliminado = cola[inicio];
+        cola[inicio] = null;
+        inicio = (inicio + 1) % cola.length;
+        numElememtos--;
+        return eliminado;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(cola);
+    }
+
 }
