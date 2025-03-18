53e2549973820c5416977f9bfcac6f5e849c81ef
santiago <147778902+sagaz-15@users.noreply.github.com>
2025-03-18 10:02:41 -0500

    Update Pilas.java

--- Proyecto/src/main/java/com/mycompany/proyecto/historial/Pilas.java
+++ Proyecto/src/main/java/com/mycompany/proyecto/historial/Pilas.java
@@ -8,6 +8,38 @@ package com.mycompany.proyecto.historial;
  *
  * @author robin
  */
-public class Pilas {
-    
+import java.util.Objects;
+
+public class Pilas<T> extends ListaDoble<T> {
+
+    public Pilas(){
+        super();
+    }
+
+    // Empty
+    public boolean empty(){
+        return cantidadNodos == 0;
+    }
+    public T peek(){
+        return head.getSig().getDato();
+    }
+    public T pop(){
+        return super.remove();
+    }
+
+    public  T push(T dato){
+        super.remove();
+        return dato;
+    }
+
+    public int search(T dato){
+        Nodo<T> nodo = head;
+        for (int i = 0; i <cantidadNodos ; i++) {
+            nodo= nodo.getSig();
+            if (Objects.equals(nodo.getDato(),dato)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
 }
